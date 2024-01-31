79a4a2b7d3b648c887914a34181e306f675cad1c
sayzs1@yeah.net <sayzs1@yeah.net>
2024-01-30 22:00:24 -0800

    update the code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from datetime import datetime
+from typing import Literal
 import streamlit as st
 from pydantic import BaseModel
 import streamlit_pydantic as sp
@@ -24,18 +25,19 @@ cur.execute(
 class Task(BaseModel):
     name: str
     description: str
-    state: str
+    state: Literal["in progress", "done"]
     created_by: str
     category: str
 
 def toggle_state(state, row):
     con = sqlite3.connect("todoapp.sqlite", isolation_level=None)
     cur = con.cursor()
+    new_state = 'in progress' if state else 'done'
     cur.execute(
         """
         UPDATE tasks SET state = ? WHERE id = ?
         """,
-        (state, row[0]),
+        (new_state, row[0]),
     )
 
 
@@ -50,7 +52,7 @@ def main():
             INSERT INTO tasks (name, description, state, created_at, created_by, category)
             VALUES (?, ?, ?, ?, ?, ?)
             """,
-            (data.name, data.description, data.state, current_time, data.category, data.created_by),
+            (data.name, data.description, data.state, current_time, data.created_by, data.category),
         )
 
     data = cur.execute(
@@ -60,21 +62,22 @@ def main():
     ).fetchall()
 
     cols = st.columns(6)
-    cols[0].write("Done?")
+    cols[0].write("State")
     cols[1].write("Name")
     cols[2].write("Description")
-    cols[3].write("State")
+    cols[3].write("Created By")
     cols[4].write("Created At")
     cols[5].write("Category")
 
     for row in data:
         cols = st.columns(6)
-        # Use 'state' instead of 'is_done' in the checkbox creation
-        cols[0].checkbox('state', row[3] == 'done', label_visibility='hidden', key=row[0], on_change=toggle_state, args=('done' if row[3] != 'done' else 'in-progress', row))
+        state_checkbox = cols[0].checkbox('state', row[3] == 'done', label_visibility='hidden', key=row[0], on_change=toggle_state, args=('done' if row[3] != 'done' else 'in-progress', row))
+        cols[0].write(row[3] if not state_checkbox else ('done' if row[3] != 'done' else 'in progress'))
         cols[1].write(row[1])
         cols[2].write(row[2])
-        cols[3].write(row[3])
+        cols[3].write(row[5])
         cols[4].write(row[4])
-        cols[5].write(row[5])
+        cols[5].write(row[6])
+
 
 main()
